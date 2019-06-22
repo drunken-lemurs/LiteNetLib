ada60dca60fcb2b64f9b47878c83cf838503e136
Nicolas Lebedenco <nicolas@lebedenco.net>
2019-06-22 12:24:41 -0400

    Removed files unnecessary for a UPM package. Added package.json and meta files.

---
+++ LiteNetLib/Properties/AssemblyInfo.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("LiteNetLib")]
+[assembly: AssemblyDescription("Lite reliable UDP library")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LiteNetLib")]
+[assembly: AssemblyCopyright("Copyright © 2019 Ruslan Pyrch")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.9.0.0")]
+[assembly: AssemblyFileVersion("0.9.0.0")]
